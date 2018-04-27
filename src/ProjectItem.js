eff476c7f11240609fe978b638178e15246a8a8f
guru.bagawan <guru.bagawan@gmail.com>
2018-04-27 13:38:45 -0400

    better project optimzation

--- src/ProjectItem.js
+++ src/ProjectItem.js
@@ -5,77 +5,77 @@ import { FlexyFlipCard } from "flexy-flipcards";
 //import Flipcard from "@kennethormandy/react-flipcard";
 
 const ProjectItem = props => {
-  return props.right ? (
-    <div
-      className={`container project-item ${
-        props.background ? "project-background" : ""
-      }`}
-    >
-      <div className="row">
-        <div className="col-md-6">
-          <img
-            className="pull-left"
-            src={props.image}
-            alt={props.title}
-            width="80%"
-          />
-        </div>
-        <div className="col-md-6">
-          <h3>{props.title} </h3>
-          <p className="project-description">
-            {props.description}
-            <br />
-          </p>
-          <h4> Learning Points </h4>
-          <p className="project-description">{props.challenges} </p>
-          {props.website && (
-            <a href={props.website} target="_blank">
-              View Online
-            </a>
-          )}
-        </div>
-      </div>
-    </div>
-  ) : (
-    <div
-      className={`container project-item ${
-        props.background ? "project-background" : ""
-      }`}
-    >
-      <div className="row">
-        <div className="col-md-6">
-          <h3>{props.title} </h3>
-          <p className="project-description">
-            {props.description}
-            <br />
-          </p>
-          <h4> Learning Points </h4>
-          <p className="project-description">{props.challenges} </p>
-          {props.website && (
-            <a href={props.website} target="_blank">
-              View Online
-            </a>
-          )}
-        </div>
-        <div className="col-md-6">
-          <img
-            className="pull-right"
-            src={props.image}
-            alt={props.title}
-            width="80%"
-          />
-        </div>
-      </div>
-    </div>
-  );
+	return props.right ? (
+		<div
+			className={`container project-item ${
+				props.background ? "project-background" : ""
+			}`}
+		>
+			<div className="row">
+				<div className="col-md-6">
+					<img
+						className="pull-left image"
+						src={props.image}
+						alt={props.title}
+						width="80%"
+					/>
+				</div>
+				<div className="col-md-6">
+					<h3>{props.title} </h3>
+					<p className="project-description">
+						{props.description}
+						<br />
+					</p>
+					<h4> Learning Points </h4>
+					<p className="project-description">{props.challenges} </p>
+					{props.website && (
+						<a href={props.website} target="_blank">
+							View Online
+						</a>
+					)}
+				</div>
+			</div>
+		</div>
+	) : (
+		<div
+			className={`container project-item ${
+				props.project.background ? "project-background" : ""
+			}`}
+		>
+			<div className="row">
+				<div className="col-md-6">
+					<h3>{props.project.title} </h3>
+					<p className="project-description">
+						{props.project.description}
+						<br />
+					</p>
+					<h4> Learning Points </h4>
+					<p className="project-description">{props.project.challenges} </p>
+					{props.project.website && (
+						<a href={props.project.website} target="_blank">
+							View Online
+						</a>
+					)}
+				</div>
+				<div className="col-md-6">
+					<img
+						className="pull-right image"
+						src={props.project.image}
+						alt={props.project.title}
+						width="80%"
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 ProjectItem.propTypes = {
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  website: PropTypes.string,
-  challenges: PropTypes.string,
-  right: PropTypes.bool
+	title: PropTypes.string.isRequired,
+	description: PropTypes.string.isRequired,
+	website: PropTypes.string,
+	challenges: PropTypes.string,
+	right: PropTypes.bool
 };
 
 export default ProjectItem;
---
+++ src/projects.js
@@ -0,0 +1,48 @@
+export const mastermind = {
+	title: "Mastermind",
+	description:
+		"A skill based game in which the user must guess a random sequence of colors based on limited feedback. Try it out, it's prety hard!",
+	image: require("./img/mastermind.png"),
+	website: "https://mastermind-game.herokuapp.com/",
+	challenges:
+		"The most challenging and fun part of this app was getting the logic sequence for the check right. It really helped solidify my knowledge of loops and conditionals"
+};
+
+export const drrobot = {
+	title: "Mastermind",
+	description:
+		"A skill based game in which the user must guess a random sequence of colors based on limited feedback. Try it out, it's prety hard!",
+	image: require("./img/mastermind.png"),
+	website: "https://mastermind-game.herokuapp.com/",
+	challenges:
+		"The most challenging and fun part of this app was getting the logic sequence for the check right. It really helped solidify my knowledge of loops and conditionals"
+};
+
+export const testarray = [
+	{
+		title: "Mastermind",
+		description:
+			"A skill based game in which the user must guess a random sequence of colors based on limited feedback. Try it out, it's prety hard!",
+		image: require("./img/mastermind.png"),
+		website: "https://mastermind-game.herokuapp.com/",
+		challenges:
+			"The most challenging and fun part of this app was getting the logic sequence for the answer review right. It really helped solidify my knowledge of loops and conditionals"
+	},
+	{
+		title: "Dr Robot",
+		description:
+			"I used API Medic to create an app that as able to diagnose the user based on the experiences they submitted as they went through the app.",
+		image: require("./img/DrRobot.png"),
+		challenges:
+			"This was my first experience in designing and building an entire app from the ground up. It was really a big challenge, but it was a really great learning experince for me. "
+	},
+	{
+		title: "Contacts App",
+		description:
+			"A contact up built with Material UI, that stores contacts by user on a Database",
+		image: require("./img/contacts.PNG"),
+		website: "https://guru-contacts-app.herokuapp.com/",
+		challenges:
+			"The GUI for this app was relatively straight forward for me to put together, however I made the app as a way to learn Firebase integration"
+	}
+];
